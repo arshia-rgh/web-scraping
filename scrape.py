3a1c4079ef9ad40eec9787300d89006d22593f88
arshia <arshiarezagholi1212@gmail.com>
2024-08-10 14:51:04 +0330

    basic fetch implemented

--- scrape.py
+++ scrape.py
@@ -1,9 +1,17 @@
 from urls.url import generate_urls
 import aiohttp
 from bs4 import BeautifulSoup
+from collections import defaultdict
+import asyncio
 
 
 class Scrape:
     urls = generate_urls()
+    html_content = defaultdict(list)
 
-    ...
+    @classmethod
+    async def fetch_html_content(cls):
+        async with aiohttp.ClientSession() as session:
+            tasks = [session.get(url) for url in cls.urls]
+            response = await asyncio.gather(*tasks)
+            return response
