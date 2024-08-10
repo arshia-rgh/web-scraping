633c43d5cdaa7070bc14045fe0fd93940ee16f52
arshia <arshiarezagholi1212@gmail.com>
2024-08-10 18:05:59 +0330

    added more methods to the Scrape

--- scrape.py
+++ scrape.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 from scrawl import scrawl_instance
+import json
 
 
 class Scrape:
     def __init__(self):
-        self.soup = {}
+        self.soup = {}  # {"urls": "soup object"}
         self.htm_content = scrawl_instance.html_content
         self.parse_html_content()
 
@@ -12,6 +13,22 @@ class Scrape:
         for url, content_list in self.htm_content.items():
             self.soup[url] = [BeautifulSoup(content, "html.parser") for content in content_list]
 
+    def extract_titles(self):
+        titles = {}
+        for url, soup_list in self.soup.items():
+            titles[url] = [soup.title.string if soup.title else "No title" for soup in soup_list]
+        return titles
 
-scrape = Scrape()
-print(scrape.soup)
+    def extract_links(self):
+        links = {}
+        for url, soup_list in self.soup.items():
+            links[url] = [a['href'] for soup in soup_list for a in soup.find_all('a', href=True)]
+        return links
+
+    @staticmethod
+    def save(data, filename):
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+
+
+scrape_instance = Scrape()
