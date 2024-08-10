73f7f44a9ed2a2f77d2c03ade4f0d5000dda19ab
arshia <arshiarezagholi1212@gmail.com>
2024-08-10 19:08:19 +0330

    added new save method to save html content too

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from scrap import scrape_instance
 
 
@@ -5,8 +7,9 @@ def main():
     links = scrape_instance.extract_links()
     titles = scrape_instance.extract_titles()
 
-    scrape_instance.save(links, "data/links.json")
-    scrape_instance.save(titles, "data/titles.json")
+    scrape_instance.save_json_extracted(links, "data/links.json")
+    scrape_instance.save_json_extracted(titles, "data/titles.json")
+    scrape_instance.save_all_html_content()
 
 
 if __name__ == "__main__":
--- scrap.py
+++ scrap.py
@@ -26,9 +26,16 @@ class Scrape:
         return links
 
     @staticmethod
-    def save(data, filename):
+    def save_json_extracted(data, filename):
         with open(filename, "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
+
+    def save_all_html_content(self):
+        for url, content_list in self.htm_content.items():
+            for i, content in enumerate(content_list):
+                filename = f"data/{url.replace('https://', '').replace('/', '_')}_{i}.html"
+                with open(filename, "w", encoding="utf-8") as f:
+                    f.write(content)
 
 
 scrape_instance = Scrape()
