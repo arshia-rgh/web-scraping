4520a756ef2587b89619997e40cdc5d69286f36e
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 15:45:21 +0330

    added test_fetch_all_html_content

--- tests/test_scrawl.py
+++ tests/test_scrawl.py
@@ -15,9 +15,13 @@ class TestScrawl:
         scrawl = Scrawl()
         assert isinstance(scrawl, Scrawl)
         assert scrawl._html_content is None
-        assert scrawl._html_content_fetched == False
+        assert scrawl._html_content_fetched is False
         assert set(scrawl.urls) == set(expected_urls)
 
     @pytest.mark.asyncio
     async def test_fetch_all_html_content(self):
-        pass
+        scrawl = Scrawl()
+
+        await scrawl.fetch_all_html_content()
+        assert scrawl._html_content_fetched is True
+        assert isinstance(scrawl._html_content, dict)
