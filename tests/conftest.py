8bdab1bff8f0b6bfdf3bec03b8fe1f050062e15b
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 15:21:53 +0330

    made conftest more formatted and added TestScrawl

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 from urls.url import Url
 
 
+#  <--- test_urls --->
 @pytest.fixture
 def url_instance():
     return Url("www.test.com")
@@ -13,3 +14,9 @@ def generate_urls_setup(monkeypatch):
     monkeypatch.setattr("urls.url.categories_list", ["car", "truck"])
     monkeypatch.setattr("urls.url.brands_list", ["toyota", "ford"])
     monkeypatch.setattr('urls.url.base_url', 'www.test.com')
+
+
+#  <--- test_scrawl --->
+@pytest.fixture
+def scrawl_setup():
+    pass
