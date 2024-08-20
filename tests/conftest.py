4917f08c5182451187c816ce8fe7f4062f629422
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 11:18:44 +0330

    added fixture for generate urls method

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,10 @@ from urls.url import Url
 @pytest.fixture
 def url_instance():
     return Url("www.test.com")
+
+
+@pytest.fixture
+def generate_urls_setup(monkeypatch):
+    monkeypatch.setattr("urls.url.categories_list", ["car", "truck"])
+    monkeypatch.setattr("urls.url.brands_list", ["toyota", "ford"])
+    monkeypatch.setattr('urls.url.base_url', 'www.test.com')
