1b996f8dcaa510d89b88273c9898e7ee1cece469
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 17:12:52 +0330

    added test for parse_html in Scrape class

--- tests/test_scrap.py
+++ tests/test_scrap.py
@@ -14,3 +14,17 @@ class TestScrape:
         assert isinstance(scrape, Scrape)
         assert scrape.soup == {}
         assert scrape.htm_content == {}
+
+    @patch("scrap.scrawl_instance")
+    def test_parse_html_content(self, mock_scrawl_instance):
+        mock_scrawl_instance.html_content = {
+            "https://example.com": [
+                "<html><head><title>Example</title></head><body></body></html>"
+            ]
+
+        }
+        scrape = Scrape()
+        assert "https://example.com" in scrape.soup
+        assert len(scrape.soup["https://example.com"]) == 1
+        assert scrape.soup["https://example.com"][0].title.string == "Example"
+
