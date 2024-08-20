99e36e3a573268ddecc7a6e119a599db93ebffd6
arshia <arshiarezagholi1212@gmail.com>
2024-08-20 15:34:45 +0330

    added test scrawl init

--- tests/test_scrawl.py
+++ tests/test_scrawl.py
@@ -1,5 +1,16 @@
 import pytest
 
+from scrawl import Scrawl
+
 
 class TestScrawl:
-    pass
+    def test_scrawl_initiate(self, generate_urls_setup):
+        expected_urls = [
+            "www.test.com/car/ford",
+            "www.test.com/car/toyota",
+            "www.test.com/truck/ford",
+            "www.test.com/truck/toyota",
+        ]
+        scrawl = Scrawl()
+        assert isinstance(scrawl, Scrawl)
+        assert set(scrawl.urls) == set(expected_urls)
